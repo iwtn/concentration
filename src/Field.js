406c433b21bc3ec369997c05ba4f2e5bdbe12d80
Yasuaki Iwata <yasuaki.iwata@livesense.co.jp>
2017-08-02 10:34:33 +0900

    use svg

--- src/Field.js
+++ src/Field.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Field extends Component {
   render() {
     return (
-      <div className="Field">
-        hogehoge
-      </div>
+      <svg width="1024" height="768">
+        <rect x="0" y="0" width="800" height="600" fill="none" stroke="blue" strokeWidth="1" />
+      </svg>
     );
   }
 }
