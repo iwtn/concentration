4a50853bd9e8c7bc721ada06830ba95e8ba4890b
Yasuaki Iwata <yasuaki.iwata@livesense.co.jp>
2017-08-02 10:56:24 +0900

    display a card on a filed

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    return (
+      <rect x="0" y="0" width="100" height="162" fill="red"  strokeWidth="0" />
+    );
+  }
+}
+
+export default Card;
--- src/Field.js
+++ src/Field.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Card from './Card.js';
 
 class Field extends Component {
   render() {
     return (
       <svg width="1024" height="768">
         <rect x="0" y="0" width="800" height="600" fill="none" stroke="blue" strokeWidth="1" />
+        <Card />
       </svg>
     );
   }
