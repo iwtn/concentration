ff2b009ce2497450de9b2b8c6879facf59036bfc
Yasuaki Iwata <yasuaki.iwata@livesense.co.jp>
2017-08-03 10:31:30 +0900

    draw 4 cards

--- src/Field.js
+++ src/Field.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import Card from './Card.js';
 
 class Field extends Component {
+  drawCards() {
+    return (
+      <g>
+        <Card x="100" y="200" />
+        <Card x="300" y="200" />
+        <Card x="100" y="400" />
+        <Card x="300" y="400" />
+      </g>
+    );
+  }
+
   render() {
     return (
       <svg width="1024" height="768">
         <rect x="0" y="0" width="800" height="600" fill="none" stroke="blue" strokeWidth="1" />
-        <Card />
+        {this.drawCards()}
       </svg>
     );
   }
