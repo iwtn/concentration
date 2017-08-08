51b7aa333fb0f8715916151393271bfebac46f08
Yasuaki Iwata <yasuaki.iwata@livesense.co.jp>
2017-08-08 10:30:55 +0900

    fix comparison operator

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Card extends Component {
   render() {
-    let color = (this.props.side == 'front') ? 'red' : 'blue';
+    let color = (this.props.side === 'front') ? 'red' : 'blue';
     return (
       <rect x={this.props.x} y={this.props.y} width="100" height="162" fill={color}  strokeWidth="0" />
     );
